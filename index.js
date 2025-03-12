6e4fdd014dee8e061ffc883e5b750a6746f627aa
Faran-back <inotfarhan@gmail.com>
2025-03-12 16:10:17 +0500

    learned how to print variable values in one go

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('My name is Faran')
+
+const EmployeeId = 11987
+let EmployeeName = 'Faran'
+let EmployeeAge = 19
+let EmployeeRole = 'Backend Web Dev'
+let EmployeeQualification = 'ICS'
+
+console.table([EmployeeId, EmployeeName, EmployeeAge, EmployeeRole, EmployeeQualification])
